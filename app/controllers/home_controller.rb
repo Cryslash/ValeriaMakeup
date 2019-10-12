881bde3cfe1428870f5ba3fd6134a2d7d06d6b90
Crystofher Lins <crystofher_lins@outlook.com>
2019-10-12 00:01:54 -0300

    hamburger menu

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
   	@social_medias = {
-      0=> ['facebook','link'],
-      1=> ['instagram','link'],
-      2=> ['whatsapp','link'] }
+      0=> ['facebook','link','fab fa-facebook-square'],
+      1=> ['instagram','link','fab fa-instagram'],
+      2=> ['whatsapp','link','fab fa-whatsapp'] }
   end
 end
