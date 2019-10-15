b8bc77dcb5e5decfa1804f77814987fd04747fe4
Crystofher Lins <crystofher_lins@outlook.com>
2019-10-14 21:48:58 -0300

    email validation

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
   get 'home/index'
   root to: 'home#index' 
+
+  #default_url_options Rails.application.config.action_mailer.default_url_options
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
