53491f321d38a209e692e7b6ba9b374130bbbd57
Crystofher Lins <crystofher_lins@outlook.com>
2019-10-28 02:19:44 -0300

    schedule time display in menu

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,11 @@ class HomeController < ApplicationController
       0=> ['facebook','link','fab fa-facebook-square'],
       1=> ['instagram','link','fab fa-instagram'],
       2=> ['whatsapp','link','fab fa-whatsapp'] }
+    
     @schedule = Schedule.new
+    
+    if current_user != nil
+      @my_schedules = Schedule.find_by :user_id => current_user.id
+    end
   end  
 end
--- db/migrate/20191021010242_create_schedules.rb
+++ db/migrate/20191021010242_create_schedules.rb
@@ -2,7 +2,7 @@ class CreateSchedules < ActiveRecord::Migration[6.0]
   def change
     create_table :schedules do |t|
       t.references :user, null: false, foreign_key: true
-      t.timestamp :date_time
+      t.date_time :date_time
 
       t.timestamps
     end
